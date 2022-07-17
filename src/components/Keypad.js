400ec9644c92ef9fc8611dd54e3db72d4b4fdafd
Nyanthie Mayang <nyanthie.mayang@student.moringaschool.com>
2022-07-17 23:34:03 +0300

    complete assignment

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
+import react from "react"
 // Code Keypad Component Here
+
+function Keypad () {
+    return <div>
+        <input
+            type="password"
+            name="password"
+            onChange={() =>(console.log("Entering password..."))} />
+
+    </div>
+}
+
+export default Keypad
